b03d39d20eb6ca1ea1ff33f57b36863e3308d157
Paul Breugnot <paul.breugnot@etu.emse.fr>
2019-03-22 09:05:58 +0100

    ActionCable config

--- config/application.rb
+++ config/application.rb
@@ -12,8 +12,9 @@ Rack::Utils.multipart_part_limit = 0
 module TheMaterialParser
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
-    config.action_cable.mount_path = '/ws'
     config.load_defaults 5.2
+
+    config.action_cable.mount_path = '/ws'
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
